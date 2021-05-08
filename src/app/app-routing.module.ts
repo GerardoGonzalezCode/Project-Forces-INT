f744c8ab2cf27b988a9f8a1fa593470634d0622a
Jerry <GerardoGonzalezCode@gmail.com>
2021-05-08 15:47:23 -0500

    Commit Inicial v1.0.0: Estructura basica de angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { DepartmentComponent } from './components/department/department.component';
+import { HomeComponent } from './components/home/home.component';
+import { RolesComponent } from './components/roles/roles.component';
+import { UserManageComponent } from './components/user-manage/user-manage.component';
+import { UserTableComponent } from './components/user-table/user-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'department', component: DepartmentComponent},
+  { path: 'roles', component: RolesComponent},
+  { path: 'manage', component: UserManageComponent},
+  { path: 'table', component: UserTableComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { UserManageComponent } from './components/user-manage/user-manage.component';
+import { DepartmentComponent } from './components/department/department.component';
+import { RolesComponent } from './components/roles/roles.component';
+import { AboutComponent } from './components/about/about.component';
+import { UserTableComponent } from './components/user-table/user-table.component';
+import { NewsService } from './services/news.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    UserManageComponent,
+    DepartmentComponent,
+    RolesComponent,
+    AboutComponent,
+    UserTableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    NewsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
